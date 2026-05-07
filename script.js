dcba6c003f25461c810e9c4a04907fb4d2a59e02
ziad <ziaddawoud86@gmail.com>
2026-05-07 22:33:02 +0300

    script.js

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 function showSection(id) {
-  // إخفاء جميع الأقسام
   var sections = document.querySelectorAll('section');
   sections.forEach(function(sec) {
     sec.classList.add('hidden');
   });
 
-  // إظهار القسم المطلوب
   document.getElementById(id).classList.remove('hidden');
 }
